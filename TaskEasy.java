5a86598ec2794c2ad60ed0cd7fd0a98e756b7b2d
MaksNat <nateki86@mail.ru>
2022-07-26 17:09:50 +0300

    Add some code

---
+++ TaskEasy.java
@@ -0,0 +1,12 @@
+package HomeWork1;
+
+import java.util.*;
+
+public class TaskEasy {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String name= sc.nextLine();
+        System.out.println("Привет, "+ name);
+
+    }
+}
---
+++ TaskNormal.java
@@ -0,0 +1,12 @@
+package HomeWork1;
+
+import java.util.Scanner;
+
+public class TaskNormal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int a = sc.nextInt(), b = sc.nextInt();
+        double a=sc.nextDouble(), b=sc.nextDouble();
+        System.out.println(a + b);
+    }
+}
